2de90d5d18562b63ae91b4ad67538240da006ee5
Adam Sorrenti <adam.sorrenti@ryerson.ca>
2020-11-11 23:19:37 -0500

    setup all variations

--- testing/tester.py
+++ testing/tester.py
@@ -22,14 +22,22 @@ token = RegexpTokenizer(r'[a-zA-Z0-9]+')
 
 classifers = [
     (MultinomialNB(), 0, 'MultinomialNB'),
-    (GaussianNB(), 0, 'GaussianNB'),
     (ComplementNB(), 0, 'ComplementNB')
 ]
 
+
 variationOptions = {
-    0 : variations.bagOfWordsAll,
-    1 : variations.tfidf,
-    2 : variations.bagOfWordsAll_NEG_wFEQ
+    0 : variations.tfidf,
+
+    1 : variations.bow,    
+    2 : variations.bow_freq,
+    3 : variations.bow_stem,
+    4 : variations.bow_stem_freq,
+    5 : variations.bow_filter,
+    6 : variations.bow_filter_freq,
+    7 : variations.bow_stem_filter,
+    8 : variations.bow_stem_filter_freq
+
 }
 
 classiferOptions = {
@@ -37,16 +45,19 @@ classiferOptions = {
     1 : classifiers.denseFit
 }
 
-def testClassifers(data, vOption=2):
-    text_t = variationOptions[vOption](data, token) # Vectorizes data based on a variation option
+def testClassifers(data, vOption=3):
+    text_t = variationOptions[vOption](data, token.tokenize) # Vectorizes data based on a variation option
     X_train, X_test, Y_train, Y_test = train_test_split(text_t, data['Rating'], test_size=0.25, random_state=5)
 
+    print(str(variationOptions[vOption]))
     for classifer, cOption, name in classifers:
         classifer, predicted = classiferOptions[cOption](classifer, X_train, Y_train, X_test) #Fits the classifer given unique option
         accuracy_score = metrics.accuracy_score(predicted, Y_test)
+        
         print(str(name+': {:04.2f}'.format(accuracy_score*100))+'%\n')
         print(metrics.confusion_matrix(Y_test, predicted))
         print('\n\n')
 
 if __name__ == "__main__":
-    testClassifers(data = pd.read_csv('dataFrame.csv')) 
+    for i in range(8):
+        testClassifers(data = pd.read_csv('dataFrame.csv'), vOption=i) 
--- testing/variations.py
+++ testing/variations.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import random
 import json
+import re
 
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB, GaussianNB, ComplementNB
@@ -9,22 +10,94 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 import matplotlib.pyplot as plt
 from nltk.tokenize import RegexpTokenizer
+from nltk.stem.porter import PorterStemmer
 
-def bagOfWordsAll(data, token):
-    cvector = CountVectorizer(stop_words='english', ngram_range=(1,1), tokenizer=token.tokenize)
-    text_t = cvector.fit_transform(data['Summary and Review'])
-    return text_t
+"""
+Helper Methods
+"""
+def filter(data):
+    def readwords(filename):
+        f = open(filename, encoding = "ISO-8859-1")
+        words = [ line.rstrip() for line in f.readlines()]
+        f.close()
+        return words
+
+    positive = readwords('positive-words.txt')
+    negative = readwords('negative-words.txt')
+    negative = negative[36:]
+    positive = positive[36:]
+    ps = {k: v for v, k in enumerate(positive)}
+    ns = {k: v for v, k in enumerate(negative)}
+
+    bog = []
+    for rev in data['Summary and Review']:
+        c = re.findall('[a-zA-Z0-9]+', rev)
+        c = [word for word in c if (word in ps or word in ns)]
+        bog.append(' '.join(c))
+    data['Summary and Review'] = bog
+    return data
+
+def stemmer(data):
+    stm = []
+    stemmer = PorterStemmer()
+    for rev in data['Summary and Review']:
+        c = re.findall('[a-zA-Z0-9]+', rev)
+        c = [stemmer.stem(plural) for plural in c]
+        stm.append(' '.join(c))
+    data['Summary and Review'] = stm
+    return data
+
+"""
+Variations
+"""
 
 def tfidf(data, token):
-    tfidf = TfidfVectorizer(stop_words='english', min_df=50, tokenizer=token.tokenize)
+    tfidf = TfidfVectorizer(stop_words='english', min_df=50, tokenizer=token)
     text_t2 = tfidf.fit_transform(data['Summary and Review'] )
     return text_t2
 
-def bagOfWordsAll_NEG_wFEQ(data, token):
+def bow(data, token):
+    cvector = CountVectorizer(stop_words='english', ngram_range=(1,1), tokenizer=token)
+    text_t = cvector.fit_transform(data['Summary and Review'])
+    return text_t
+
+def bow_filter(data, token):
+    data = filter(data)
+    cvector = CountVectorizer(stop_words='english', ngram_range=(1,1))
+    text_t = cvector.fit_transform(data['Summary and Review'])
+    return text_t
+
+def bow_filter_freq(data, token):
+    data = filter(data)
+    return bow_freq(data, None)
+
+def bow_stem(data, token):
+    data = stemmer(data)
+    cvector = CountVectorizer(stop_words='english', ngram_range=(1,1))
+    text_t = cvector.fit_transform(data['Summary and Review'])
+    return text_t
+
+def bow_stem_freq(data, token):
+    data = stemmer(data)
+    return bow_freq(data, None)
+
+def bow_stem_filter(data, token):
+    data = filter(data)
+    data = stemmer(data)
+    cvector = CountVectorizer(stop_words='english', ngram_range=(1,1))
+    text_t = cvector.fit_transform(data['Summary and Review'])
+    return text_t
+
+def bow_stem_filter_freq(data, token):
+    data = filter(data)
+    data = stemmer(data)
+    return bow_freq(data, None)
+    
+def bow_freq(data, token):
     '''
-    Negation code from eisa using positive-words.txt and negative-words.txt
+    Appends positive negative word frequencies to each review
     '''
-    mainData = bagOfWordsAll(data, token)
+    mainData = bow(data, token)
     sample = []
     for i in range(len(data)):
         temp = ''.join(data.iloc[i]['Summary and Review'].split('\n')).split()
